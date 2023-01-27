72360a74f9ddfc349a34f8ef4c33fb4a45de3a3a
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2023-01-27 17:29:53 +0900

    fix container name in run_migrate_in_host.sh

--- run_migrate_in_host.sh
+++ run_migrate_in_host.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it purl_web /bin/bash -c "export PATH=/usr/local/rbenv/shims:/usr/local/rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin; bundle exec rake db:migrate"
+docker exec -it purl_web_apache /bin/bash -c "export PATH=/usr/local/rbenv/shims:/usr/local/rbenv/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin; bundle exec rake db:migrate"
