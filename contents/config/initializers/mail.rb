0c7d8040b554b93bb39167b8e26416e5cdee79e0
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2023-01-28 22:24:15 +0900

    update IP and port of mail server

--- contents/config/initializers/mail.rb
+++ contents/config/initializers/mail.rb
@@ -2,15 +2,15 @@ if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    address: '192.168.1.5',
-    port: 1025,
+    address: '172.18.8.205',
+    port: 25,
 }
 elsif Rails.env.development?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings = {
-    address: '192.168.1.5',
-    port: 1025,
+    address: '172.18.8.205',
+    port: 25,
   }
   #ActionMailer::Base.delivery_method = :letter_opener
 else
